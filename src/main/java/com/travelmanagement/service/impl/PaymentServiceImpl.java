d40564e82d3738024b38882c45fdc15cd9858a46
ram-sirvi10 <ramsirvi10@gmail.com>
2025-09-16 19:16:45 +0530

    Solve Conflict And merge Code

--- src/main/java/com/travelmanagement/service/impl/PaymentServiceImpl.java
+++ src/main/java/com/travelmanagement/service/impl/PaymentServiceImpl.java
@@ -1,7 +1,46 @@
 package com.travelmanagement.service.impl;
 
+import java.util.List;
+
+import com.travelmanagement.model.Agency;
 import com.travelmanagement.service.IPaymentService;
 
 public class PaymentServiceImpl implements IPaymentService{
+
+	@Override
+	public Agency createPackage(Agency p) throws Exception {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Agency getById(int id) throws Exception {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<Agency> getAll() throws Exception {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<Agency> search(String keyword) throws Exception {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public boolean updatePackage(Agency p) throws Exception {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean deletePackage(int id) throws Exception {
+		// TODO Auto-generated method stub
+		return false;
+	}
 
 }
