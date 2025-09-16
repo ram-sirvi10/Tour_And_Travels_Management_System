20d2292a8de8db000c24b50903093e6224fb852f
ram-sirvi10 <ramsirvi10@gmail.com>
2025-09-16 11:28:34 +0530

    update folder architecture - date 16/09/25

---
+++ src/main/java/com/travelmanagement/service/IAuthService.java
@@ -0,0 +1,5 @@
+package com.travelmanagement.service;
+
+public interface IAuthService {
+
+}
--- src/main/java/com/travelmanagement/service/impl/UserServiceImpl.java
+++ src/main/java/com/travelmanagement/service/impl/UserServiceImpl.java
@@ -1,5 +1,14 @@
 package com.travelmanagement.service.impl;
 
+import com.travelmanagement.dto.requestDTO.RegisterRequestDTO;
+import com.travelmanagement.model.User;
 public class UserServiceImpl  {
+	
+	public User mapRegisterDtoToUser(RegisterRequestDTO request)
+	{
+		 User user = new User();
+		 user.setUserName(request.getUsername());
+		 return user;
+	}
 
 }
